be40591499e4a8bc3aa736afe018224352ead2fc
Kogane <heughebaert.dev@gmail.com>
2021-10-15 20:35:06 +0200

    Update UI_MainMenuClass

--- Source/DungeonsThief/HUD/UI_MainMenuClass.cpp
+++ Source/DungeonsThief/HUD/UI_MainMenuClass.cpp
@@ -3,4 +3,51 @@
 
 #include "DungeonsThief/HUD/UI_MainMenuClass.h"
 
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void UUI_MainMenuClass::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	
+	BtnPlay->OnClicked.AddUniqueDynamic(this, &UUI_MainMenuClass::PlayNormalGame);
+	BtnPlayScore->OnClicked.AddUniqueDynamic(this, &UUI_MainMenuClass::PlayScoreGame);
+	BtnSettings->OnClicked.AddUniqueDynamic(this, &UUI_MainMenuClass::OpenSettings);
+	BtnExit->OnClicked.AddUniqueDynamic(this, &UUI_MainMenuClass::ExitGame);
+
+	UE_LOG(LogTemp, Warning, TEXT("INIT"));
+}
+
+void UUI_MainMenuClass::PlayNormalGame()
+{
+	UWorld* World = GetWorld();
+	UE_LOG(LogTemp, Warning, TEXT("START GAME"));
+
+	if (World)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("START GAME"));
+		UGameplayStatics::SetGamePaused(World, false);
+		UGameplayStatics::OpenLevel(World, LevelToLoadName);
+	}
+}
+
+void UUI_MainMenuClass::PlayScoreGame()
+{
+	UE_LOG(LogTemp, Warning, TEXT("START SCORE GAME"));
+}
+
+
+void UUI_MainMenuClass::OpenSettings()
+{
+	UE_LOG(LogTemp, Warning, TEXT("OPEN SETTINGS"));
+}
+
+void UUI_MainMenuClass::ExitGame()
+{
+	GetWorld()->GetFirstPlayerController()->ConsoleCommand("quit");
+	UE_LOG(LogTemp, Warning, TEXT("EXIT GAME"));
+}
+
 
